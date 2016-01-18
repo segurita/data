f2c22ed7ca8a5ae7c0e7f21c57fc3e9915a36776
aniversarioperu <yoni@aniversarioperu.me>
2016-01-17 23:08:55 -0500

    improve tests

--- tests.py
+++ tests.py
@@ -1,8 +1,14 @@
+import glob
+import os
 import unittest
 
 from validate import validate_data
 
 
 class TestSecurity(unittest.TestCase):
-    def test_continc1(self):
-        self.assertEqual("Valid", validate_data("continc1.json"))
+    def setUp(self):
+        self.json_files = [filename for filename in glob.glob(os.path.join("data", "*json"))]
+
+    def test_data(self):
+        for filename in self.json_files:
+            self.assertEqual("Valid", validate_data(filename), filename)
